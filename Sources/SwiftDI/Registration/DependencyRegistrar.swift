d2ffcc0563f1794a20f0729c3f99ac49a27eeda8
Faizan Tanveer <dev.ftanveer@icloud.com>
2026-07-18 07:08:27 +0500

    added dependency Registrar

---
+++ Sources/SwiftDI/Registration/DependencyRegistrar.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  SwiftDI
+//
+//  Created by Faizan Tanveer on 18/07/2026.
+//
+
+import Foundation
+
+protocol DependencyRegistrar {
+    func registerAllDependencies(in container: DIContainer)
+}
